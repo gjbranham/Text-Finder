5ca5c322c4414abc63f744722d701018ac8c124e
Griffin Branham <griffin@apricitycode.com>
2023-10-03 20:43:58 -0700

    gitignore. speed up search by shrinking chunk size.

--- files.go
+++ files.go
@@ -16,20 +16,25 @@ func getAllFiles(root string) []string {
 	fileList := []string{}
 
 	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
-		if d.IsDir() || strings.Contains(path, "go-text-finder") || strings.Contains(path, "Documents/Bitfloor") || slices.Contains(getIgnoreExts(), filepath.Ext(path)) {
+		ex, e := os.Executable()
+		if e != nil {
+			log.Fatal("Fatal error: could not determine path name for running executable")
+		}
+		exPath := filepath.Dir(ex)
+		if d.IsDir() || strings.Contains(path, exPath) || strings.Contains(path, "Documents/Bitfloor") || slices.Contains(getIgnoreExts(), filepath.Ext(path)) {
 			return nil
 		}
 		fileList = append(fileList, path)
 		return nil
 	})
 	if err != nil {
-		log.Fatalf("Failure occurred while walking directories: %v", err)
+		log.Fatalf("Fatal error occurred while walking directories: %v", err)
 	}
 	return fileList
 }
 
 func checkFilesForMatch(fileList []string, result chan string, numMatches chan int) {
-	chunk := 8 * 1024 * 1024
+	chunk := 1024 * 1024
 	buf := make([]byte, chunk)
 
 	mflag := 0
