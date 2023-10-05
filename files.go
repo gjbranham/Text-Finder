3c7cebba50f4fdffe4ae27a2e32807e3cbc2b186
Griffin Branham <griffin@apricitycode.com>
2023-10-04 22:24:59 -0700

    Simplify, remove channels, add mutex counter and waitgroups. Cut runtime roughly in half

--- files.go
+++ files.go
@@ -8,12 +8,13 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"golang.org/x/exp/slices"
 )
 
-func getAllFiles(root string) []string {
-	fileList := []string{}
+func getAllFiles(root string) {
+	var wg sync.WaitGroup
 
 	ex, e := os.Executable()
 	if e != nil {
@@ -30,51 +31,49 @@ func getAllFiles(root string) []string {
 		if d.IsDir() || strings.Contains(path, exPath) || (info.Mode()&os.ModeSymlink) == os.ModeSymlink || slices.Contains(getIgnoreExts(), filepath.Ext(path)) {
 			return nil
 		}
-		fileList = append(fileList, path)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			checkFileForMatch(path)
+		}()
 		return nil
 	})
 	if err != nil {
 		log.Fatalf("Fatal error occurred while walking directories: %v", err)
 	}
-	return fileList
+	wg.Wait()
 }
 
-func checkFilesForMatch(fileList []string, result chan string, numMatches chan int) {
+func checkFileForMatch(file string) {
 	chunk := 1024 * 1024
 	buf := make([]byte, chunk)
 
 	mflag := 0
-	matches := 0
 
-	for _, file := range fileList {
-		fileObj, err := os.Open(file)
+	fileObj, err := os.Open(file)
+	if err != nil {
+		log.Printf("Failed to open file '%v", file)
+	}
+	defer fileObj.Close()
+
+	for {
+		bytesRead, err := fileObj.Read(buf)
 		if err != nil {
-			log.Printf("Failed to open file '%v", file)
+			if err != io.EOF {
+				log.Printf("Failed to read chunk from file: %v", err.Error())
+			}
+			break
 		}
-		defer fileObj.Close()
-
-		for {
-			bytesRead, err := fileObj.Read(buf)
-			if err != nil {
-				if err != io.EOF {
-					log.Printf("Failed to read chunk from file: %v", err.Error())
-				}
+		for _, keyword := range getSearchStrings() {
+			if strings.Contains(string(buf[:bytesRead]), keyword) {
+				fmt.Printf("%v\n", file)
+				matchCounter.inc()
+				mflag = 1
 				break
 			}
-			for _, keyword := range getSearchStrings() {
-				if strings.Contains(string(buf[:bytesRead]), keyword) {
-					fmt.Printf("%v\n", file)
-					matches++
-					result <- file
-					mflag = 1
-					break
-				}
-			}
-			if mflag == 1 {
-				mflag = 0
-				break
-			}
+		}
+		if mflag == 1 {
+			break
 		}
 	}
-	numMatches <- matches
 }
--- main.go
+++ main.go
@@ -2,43 +2,29 @@ package main
 
 import (
 	"log"
+	"sync"
 	"time"
 )
+
+type mutexCounter struct {
+	mu    sync.Mutex
+	count int
+}
+
+func (c *mutexCounter) inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.count++
+}
+
+var matchCounter *mutexCounter = new(mutexCounter)
 
 func main() {
 	startDir, _ := processArgs()
 
 	start := time.Now()
 
-	fileList := getAllFiles(startDir)
+	getAllFiles(startDir)
 
-	log.Printf("Found %v matches in %v", len(fileList), time.Since(start))
-
-	divisor := 64
-	chunk := len(fileList) / divisor
-	remainder := len(fileList) - (divisor * chunk)
-
-	fileMatchCh := make(chan string, len(fileList))
-	matchCountCh := make(chan int)
-
-	start = time.Now()
-
-	for i := 0; i < divisor; i++ {
-		go checkFilesForMatch(fileList[i*chunk:(i+1)*chunk], fileMatchCh, matchCountCh)
-	}
-	// Need to check the remainder N files as well
-	go checkFilesForMatch(fileList[len(fileList)-remainder:], fileMatchCh, matchCountCh)
-
-	matches := 0
-	for i := 0; i < divisor+1; i++ {
-		m := <-matchCountCh
-		matches += m
-	}
-
-	matchedFiles := make([]string, 0)
-	for i := 0; i < matches; i++ {
-		f := <-fileMatchCh
-		matchedFiles = append(matchedFiles, f)
-	}
-	log.Printf("Found %v matching files in %v", len(matchedFiles), time.Since(start))
+	log.Printf("Found %v matching files in %v", matchCounter.count, time.Since(start))
 }
