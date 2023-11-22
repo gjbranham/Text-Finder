e3994fcf20c9645c9a9ba60a79b7ac814b20454f
Griffin Branham <griffin@apricitycode.com>
2023-11-21 20:49:26 -0800

    Fix tableized tests and add conditions for multi matches

--- internal/application/search_test.go
+++ internal/application/search_test.go
@@ -2,9 +2,12 @@ package application
 
 import (
 	"bytes"
+	"errors"
+	"io/fs"
 	"log"
 	"os"
 	"path"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -34,74 +37,151 @@ func TestMain(m *testing.M) {
 
 func TestSimpleCmdLineArguments(t *testing.T) {
 	type test struct {
-		args         []string
-		content      string
-		matches      int
-		matchingFile string
-		matchingKey  string
-		matchingLine int
+		args              []string
+		fileContent       string
+		expectedMatchInfo c.MatchInformation
 	}
 
 	tests := []test{
 		{
-			args: []string{"myapp", "-d", testDir, "foo"}, content: "foo", matches: 1, matchingFile: "simpleSearch", matchingKey: "foo", matchingLine: 1,
+			args:        []string{"myapp", "-d", testDir, "foo"},
+			fileContent: "foo",
+			expectedMatchInfo: c.MatchInformation{
+				Count: 1,
+				Matches: []c.FileInfo{
+					{Key: "foo", File: path.Join(testDir, "simpleSearch"), LineNum: 1},
+				},
+			},
 		},
 		{
-			args: []string{"myapp", "-i", "-d", testDir, "Foo"}, content: "foo", matches: 1, matchingFile: "caseInsensitive", matchingKey: "Foo", matchingLine: 1,
+			args:        []string{"myapp", "-i", "-d", testDir, "Foo"},
+			fileContent: "foo",
+			expectedMatchInfo: c.MatchInformation{
+				Count: 1,
+				Matches: []c.FileInfo{
+					{Key: "Foo", File: path.Join(testDir, "caseInsensitiveSearch"), LineNum: 1},
+				},
+			},
 		},
 		{
-			args: []string{"myapp", "-d", "", "foo"}, content: "foo", matches: 1, matchingFile: "emptyRootDir", matchingKey: "foo", matchingLine: 1,
+			args:        []string{"myapp", "-i", "-d", "", "foo"},
+			fileContent: "foo",
+			expectedMatchInfo: c.MatchInformation{
+				Count: 1,
+				Matches: []c.FileInfo{
+					{Key: "foo", File: path.Join(testDir, "emptyRootDir"), LineNum: 1},
+				},
+			},
 		},
 		{
-			args: []string{"myapp", "-d", testDir}, content: "foo", matches: 0, matchingFile: "noSearchTerms",
+			args:        []string{"myapp", "-r", "-d", testDir, "foo"},
+			fileContent: "foo",
+			expectedMatchInfo: c.MatchInformation{
+				Count: 1,
+				Matches: []c.FileInfo{
+					{Key: "foo", File: path.Join(testDir, "recursiveDir/recursiveSearch"), LineNum: 1},
+				},
+			},
 		},
 		{
-			args: []string{"myapp", "-d", testDir, "foo"}, content: "foo" + string([]byte{0}), matches: 0, matchingFile: "binaryFile",
+			// no search terms
+			args:        []string{"myapp", "-d", testDir},
+			fileContent: "",
+			expectedMatchInfo: c.MatchInformation{
+				Count:   0,
+				Matches: []c.FileInfo{},
+			},
 		},
 		{
-			args: []string{"myapp", "-r", "-d", testDir, "foo"}, content: "foo", matches: 1, matchingFile: "recursiveDir/recursiveSearch", matchingKey: "foo", matchingLine: 1,
+			// binary file
+			args:        []string{"myapp", "-d", testDir},
+			fileContent: "foo" + string([]byte{0}),
+			expectedMatchInfo: c.MatchInformation{
+				Count:   0,
+				Matches: []c.FileInfo{},
+			},
+		},
+		{
+			args:        []string{"myapp", "-d", testDir, "foo"},
+			fileContent: "foo",
+			expectedMatchInfo: c.MatchInformation{
+				Count: 2,
+				Matches: []c.FileInfo{
+					{Key: "foo", File: path.Join(testDir, "firstMatchingFile"), LineNum: 1},
+					{Key: "foo", File: path.Join(testDir, "secondMatchingFile"), LineNum: 1},
+				},
+			},
+		},
+		{
+			args:        []string{"myapp", "-d", testDir, "foo"},
+			fileContent: "foo\n",
+			expectedMatchInfo: c.MatchInformation{
+				Count: 2,
+				Matches: []c.FileInfo{
+					{Key: "foo", File: path.Join(testDir, "multiLineMatches"), LineNum: 1},
+					{Key: "foo", File: path.Join(testDir, "multiLineMatches"), LineNum: 2},
+				},
+			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(strings.Join(tt.args, " "), func(t *testing.T) {
-			writeTestFile(tt.matchingFile, tt.content)
+			for _, m := range tt.expectedMatchInfo.Matches {
+				writeTestFile(m.File, tt.fileContent)
+			}
 			os.Args = tt.args
 			args, _, _ := args.ProcessArgs(os.Args[0], os.Args[1:])
 			app := TextFinder{Args: args, MatchInfo: new(c.MatchInformation)}
 			app.FindFiles(testDir)
 
-			if len(app.MatchInfo.Matches) != tt.matches {
-				t.Errorf("Wrong number of file matches\nExpected: %v\nGot:      %v\n", tt.matches, len(app.MatchInfo.Matches))
+			if err := checkMatch(tt.expectedMatchInfo, *app.MatchInfo); err != nil {
+				t.Errorf("Test failed: %v", err)
 			}
-
-			if len(app.MatchInfo.Matches) > 0 {
-				matchInfo := app.MatchInfo.Matches[0]
-				if matchInfo.File != path.Join(testDir, tt.matchingFile) {
-					t.Errorf("File found does not match expected\nExpected: %v\nGot:      %v\n", tt.matchingFile, matchInfo.File)
-				}
-				if matchInfo.Key != tt.matchingKey {
-					t.Errorf("Search terms do not match expected\nExpected: %v\nGot:      %v\n", tt.matchingKey, matchInfo.Key)
-				}
-				if matchInfo.LineNum != tt.matchingLine {
-					t.Errorf("Wrong line number saved for match\nExpected: %v\nGot:      %v\n", tt.matchingLine, matchInfo.LineNum)
-				}
+			for _, m := range tt.expectedMatchInfo.Matches {
+				removeTestFile(m.File)
 			}
-			removeTestFile(tt.matchingFile)
 		})
 	}
 }
 
-func writeTestFile(name string, content string) {
-	path := path.Join(testDir, name)
-	if err := os.WriteFile(path, []byte(content), 0777); err != nil {
-		log.Fatalf("Failed to write file: %v\n", err)
+func checkMatch(expected, actual c.MatchInformation) error {
+	if expected.Count != actual.Count {
+		return errors.New("matching file count did not match")
 	}
+	if len(expected.Matches) == 0 && len(actual.Matches) == 0 { // for searches that return no results
+		return nil
+	}
+	for _, a := range actual.Matches {
+		for _, e := range expected.Matches {
+			if reflect.DeepEqual(a, e) {
+				return nil
+			}
+		}
+	}
+	return errors.New("file match not found")
 }
 
-func removeTestFile(name string) {
-	path := path.Join(testDir, name)
-	if err := os.Remove(path); err != nil {
+func writeTestFile(filePath string, content string) {
+	fo, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("Failed to open file for writing: %v\n", err)
+	}
+
+	if _, err := fo.Write([]byte(content)); err != nil {
+		log.Fatalf("Failed to write file: %v\n", err)
+	}
+
+	// if err := os.WriteFile(filePath, []byte(content), 0777); err != nil {
+	// 	log.Fatalf("Failed to write file: %v\n", err)
+	// }
+}
+
+func removeTestFile(filePath string) {
+	if _, err := os.Stat(filePath); errors.Is(err, fs.ErrNotExist) {
+		return
+	}
+	if err := os.Remove(filePath); err != nil {
 		log.Fatalf("Failed to remove file: %v\n", err)
 	}
 }
